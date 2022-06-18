3224f0376a872bf85a0f9b1e7e12a11088e31ee4
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-06-18 23:14:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-"use strict";
+" use strict";
 
-function guessingNumber() {
-  let randomNumber = Math.floor(Math.random() * 9 + 1);
-  console.log(randomNumber);
-  const guessNumber = function () {
-    let userNumber = +prompt("Угадай число от 1 до 10");
-    console.log(userNumber);
+// Восстановить порядок книг
+const books = document.querySelectorAll(".books");
 
-    if (isNaN(userNumber) || userNumber === "") {
-      alert("Введите число");
-      return guessNumber();
-    }
+const book = document.querySelectorAll(".book");
 
-    if (userNumber === null) {
-      alert("Игра окончена");
-      return;
-    }
+book[0].before(book[1]);
+book[3].before(book[4]);
+book[2].remove();
+books[0].append(book[2]);
 
-    if (userNumber !== randomNumber) {
-      if (userNumber > randomNumber) {
-        alert("Загаданное число меньше!");
-        return guessNumber();
-      } else if (userNumber < randomNumber) {
-        alert("Загаданное число больше!");
-        return guessNumber();
-      }
-    } else {
-      alert("Поздравляю, Вы угадали!");
-    }
+console.log(book);
 
-    return;
-  };
-  guessNumber();
-}
-guessingNumber();
+// Заменить картинку заднего фона
+const body = document.querySelector("body");
+body.style.backgroundImage = 'url("./image/you-dont-know-js.jpg")';
+console.log(body);
+console.dir(body.style);
+
+//Исправить заголовок в книге 3
+const titleBooks = document.querySelectorAll("h2");
+titleBooks[2].querySelector("a").textContent =
+  "Книга 3. this и Прототипы Объектов";
+
+//Удалить рекламу со страницы
+const adv = document.querySelector(".adv");
+adv.remove();
+console.log(adv);
+
+//Восстановить порядок глав во второй и пятой книге
+const links2 = book[0].querySelectorAll("li");
+// links[3].insertAdjacentElement("afterend", "links[6]");
+console.log(links2);
+links2[3].after(links2[6]);
+links2[6].after(links2[8]);
+const links5 = book[5].querySelectorAll("li");
+console.log(links5);
+links5[3].before(links5[9]);
+links5[5].after(links5[2]);
+
+//в шестой книге добавить главу
+const links6 = book[2].querySelectorAll("li");
+const newLink = document.createElement("li");
+newLink.textContent = "Глава 8: За пределами ES6";
+links6[8].after(newLink);
