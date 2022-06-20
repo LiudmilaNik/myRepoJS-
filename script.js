4cee9ec5c75c3c5988bb89e877cda46f5b52f7e4
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-06-20 17:34:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-"use strict";
+" use strict";
 
-function guessingNumber() {
-  let randomNumber = Math.floor(Math.random() * 9 + 1);
-  console.log(randomNumber);
-  const guessNumber = function () {
-    let userNumber = +prompt("Угадай число от 1 до 10");
-    console.log(userNumber);
+const btn = document.getElementById("btn");
 
-    if (isNaN(userNumber) || userNumber === "") {
-      alert("Введите число");
-      return guessNumber();
-    }
-
-    if (userNumber === null) {
-      alert("Игра окончена");
-      return;
-    }
-
-    if (userNumber !== randomNumber) {
-      if (userNumber > randomNumber) {
-        alert("Загаданное число меньше!");
-        return guessNumber();
-      } else if (userNumber < randomNumber) {
-        alert("Загаданное число больше!");
-        return guessNumber();
-      }
-    } else {
-      alert("Поздравляю, Вы угадали!");
-    }
-
-    return;
-  };
-  guessNumber();
-}
-guessingNumber();
+btn.addEventListener("click", function (event) {
+  const input = document.querySelector("input").value;
+  const square = document.getElementById("square");
+  square.style.backgroundColor = input;
+  console.log(input);
+});
