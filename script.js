7965cef273df19e8c27f6f47e9b41998c2f88d48
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-06-20 21:25:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 " use strict";
-
+//
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function (event) {
-  const input = document.querySelector("input").value;
+  const input = document.getElementById("text").value;
   const square = document.getElementById("square");
   square.style.backgroundColor = input;
   console.log(input);
 });
+
+const e_btn = document.getElementById("e_btn");
+console.log(e_btn);
+e_btn.style = "display:none";
+
+const range = document.getElementById("range");
+range.addEventListener("change", function (event) {
+  const circle = document.getElementById("circle");
+  circle.style.width = range.value + "%";
+  circle.style.height = range.value + "%";
+  const rangeSpan = document.querySelector("span");
+  rangeSpan.textContent = range.value;
+  console.log(rangeSpan);
+});
+console.log(circle);
