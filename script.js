4beb773661daabc7cc5ff66c05987ae42176243f
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-07-04 00:18:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
 "use strict";
 
-function guessingNumber() {
-  let randomNumber = Math.floor(Math.random() * 9 + 1);
-  console.log(randomNumber);
-  const guessNumber = function () {
-    let userNumber = +prompt("Угадай число от 1 до 10");
-    console.log(userNumber);
-
-    if (isNaN(userNumber) || userNumber === "") {
-      alert("Введите число");
-      return guessNumber();
-    }
-
-    if (userNumber === null) {
-      alert("Игра окончена");
-      return;
-    }
-
-    if (userNumber !== randomNumber) {
-      if (userNumber > randomNumber) {
-        alert("Загаданное число меньше!");
-        return guessNumber();
-      } else if (userNumber < randomNumber) {
-        alert("Загаданное число больше!");
-        return guessNumber();
-      }
-    } else {
-      alert("Поздравляю, Вы угадали!");
-    }
-
-    return;
-  };
-  guessNumber();
+class First {
+  // constructor() {}
+  static hello() {
+    console.log(`Привет, я метод родителя!`);
+  }
 }
-guessingNumber();
+
+class Second extends First {
+  // constructor() {}
+  static hello() {
+    super.hello();
+    console.log(`А я - наследуемый метод!`);
+  }
+}
+
+Second.hello();
